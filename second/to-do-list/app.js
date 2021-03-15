39c124c6f72d8dc8e8be9f16a4c15b2a699b0a0c
panLukaszGelmintowski <72591420+panLukaszGelmintowski@users.noreply.github.com>
2021-03-15 09:49:39 +0500

    optimized, third in work

--- second/to-do-list/app.js
+++ second/to-do-list/app.js
@@ -1,39 +1,31 @@
-//selectors
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-//event functions
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 
-//functions
-
 function addTodo(event) {
     if (todoInput.value == ""){
+        event.preventDefault();
         alert('невалидно');
     }
     else{    
         event.preventDefault();
-        //todo div
         const todoDiv = document.createElement("div");
         todoDiv.classList.add("todo");
-        //create li
         const newTodo = document.createElement("li");
         newTodo.innerText = todoInput.value;
         newTodo.classList.add("todo-item");
         todoDiv.appendChild(newTodo);
-        //checkmark buttton
         const completedButton = document.createElement("button");
         completedButton.innerHTML = '<i class="fas fa-check"></i>';
         completedButton.classList.add("complete-btn");
         todoDiv.appendChild(completedButton);
-        //checktrash buttton
         const trashButton = document.createElement("button");
         trashButton.innerHTML = '<i class="fas fa-trash"></i>';
         trashButton.classList.add("trash-btn");
         todoDiv.appendChild(trashButton);
-        //append to list
         todoList.appendChild(todoDiv);
         todoInput.value = "";
     }
