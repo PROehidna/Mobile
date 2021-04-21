eddd354a7adaba0f6e858ce3b28a6e6a0c1ef80c
panLukaszGelmintowski <72591420+panLukaszGelmintowski@users.noreply.github.com>
2021-04-22 02:04:59 +0500

    third is shitty but it work

---
+++ third/todo/app.js
@@ -0,0 +1,24 @@
+var vue = new Vue({
+    el: '#app',
+    data: {
+        newTodo: '',
+        todos: [],
+        done: false,
+    },
+    methods: {
+        newElem: function() {
+            if (this.newTodo == ''){
+                alert('ошибка');
+            }
+            else{
+                this.todos.push({
+                    title: this.newTodo,
+                });
+                this.newTodo = '';
+            }
+        },
+        removeElem: function(todo) {
+            this.todos.splice(this.todos.indexOf(todo), 1);
+        },
+    }
+})
