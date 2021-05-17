5c1c55ab385d4df6a6989950245a45c8834ffbed
panLukaszGelmintowski <vsevolod.doronin@yandex.ru>
2021-05-17 23:09:58 +0500

    icon changed

--- LifeSim/src/components/App.vue
+++ LifeSim/src/components/App.vue
@@ -1,17 +1,40 @@
 <template>
-    <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
-        <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
+    <Page> 
+        <ActionBar title="Yet Another Life Sim"/>
+        <StackLayout>
+            <Button class="message" text="новая игра"/>
+            <Button class="message" text="продолжить"/>
+            <Button class="message" text="загрузить игру"/>            
+            <Button class="message" text="настройки"/>
+            <Button class="message" @tap="about()" text="о"/>
+            <Button class="message" @tap="closeApp()" text="выход"/>
+        </StackLayout>
     </Page>
 </template>
 
 <script >
+import * as application from "@nativescript/core/application";
+
   export default {
     data() {
       return {
         msg: 'Hello World!'
+      }
+    },
+    methods:{
+      about() {
+        alert(({
+          title: "о",
+          message: "о",
+          okButtonText: "о"
+        }));
+      },
+      closeApp() {
+        if (application.android) {
+          application.android.foregroundActivity.finish();
+        } else {
+          exit(0);
+        }
       }
     }
   }
@@ -19,14 +42,23 @@
 
 <style scoped>
     ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
+        background-color: #B9D7EA;
+        color: rgb(41, 41, 41);
     }
-
+    Page {
+        background-color: #F7FBFC;
+    }
     .message {
         vertical-align: center;
         text-align: center;
         font-size: 20;
-        color: #333333;
+        color: #111111;
+        background-color: #D6E6F2;
+        font-size: 16px;
+        font-family: sans-serif;
+        width: 75%;
+        height: 36rem;
+        margin-top: 12rem;
+        border-radius: 15%;
     }
 </style>
